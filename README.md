414944c87204cad080b4e97188651551dd44432b
brainfo <scilavisher@gmail.com>
2025-05-13 15:17:49 +0200

    add customized barh plot

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A toolkit for genetic analysis.
 ## Installation
 
 ```bash
+pip install git+https://github.com/brainfo/plutils.git
 pip install genetk
 ```
 
@@ -12,7 +13,60 @@ pip install genetk
 
 - Use gseapy for gene set enrichment analysis
   - given gtf file, annotate and select only protein-coding genes for enrichment
+  - The gseapy plotting utils support mapping q values to colors for dot plots but not barplots. They have barplots with fixed float (p or q values) as one axis, and at most color by database groups.
+    - thus, here's a simple barplot function for flexibly choosing field as axis and mapping colors
+  - Support both explorary saving out all sig terms directly from fresh analysis, or pre-saved tables
+    - for pre-saved tables, the plot saving is boosted with plutils package 
 - Use magma to enrich for GWAS hits
 - Use networkx for Network analysis given any edge list
   - In-degree, Out-degree, and Betweenness centrality
   - Find communities
+
+## Example Usage
+
+```python
+import pandas as pd
+import numpy as np
+
+
+data = {
+    'Gene_set': ['GO_Biological_Process_2023'] * 5,
+    'Term': [
+        'Regulation Of Canonical Wnt Signaling Pathway (GO:0060828)',
+        'Vascular Transport (GO:0010232)',
+        'Positive Regulation Of Telomere Maintenance Via Telomere Lengthening (GO:1904358)'
+    ],
+    'Overlap': ['6/207', '4/83', '3/35'],
+    'P-value': [
+        0.0002375382634268994,
+        0.00041336149822381393,
+        0.0004244897820241493
+    ],
+    'Adjusted P-value': [
+        0.0386193554324011,
+        0.0386193554324011,
+        0.0386193554324011
+    ],
+    'Old P-value': [0, 0, 0],
+    'Old Adjusted P-value': [0, 0, 0],
+    'Odds Ratio': [
+        7.544967470340604,
+        12.55506329113924,
+        23.01388888888889
+    ],
+    'Combined Score': [
+        62.96412550405352,
+        97.81885907452366,
+        178.6941623125691
+    ],
+    'Genes': [
+        'KANK1;ZNRF3;WNK2;USP34;LGR5;RBMS3',
+        'SLC13A3;ABCB1;SLC24A3;LRP2',
+        'PKIB;HNRNPA2B1;MAP3K4'
+    ]
+}
+
+df = pd.DataFrame(data)
+
+genetk.enrich.go.barh(df, 'GO', 'down', 'exact', save = 'test.pdf', return_ax=False)
+```
--- src/genetk/enrich/go.py
+++ src/genetk/enrich/go.py
@@ -6,88 +6,47 @@ import numpy as np
 import seaborn as sns
 import __future__
 import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.rcParams['pdf.fonttype'] = 42
-matplotlib.rcParams['ps.fonttype'] = 42
-matplotlib.rcParams['font.family'] = ['sans-serif']
-matplotlib.rcParams['font.sans-serif'] = ['Arial']
-matplotlib.rcParams.update({'font.size': 18})
+import plutils as plu
 
-# def plot_GO(fname, name, term):
-    
-#     df = pd.read_csv(fname,sep='\t')
-#     df['Adjusted P-value'].astype(float)
-#     df = df.loc[df['Adjusted P-value']<0.05]
-#     if term == 'MP':
-#         term_name = [' '.join(segment.split(' ')[:-1]) for segment in df.Term]
-#     else:
-#         term_name = [segment.split('(')[0] for segment in df.Term]
-#     term_name.reverse()
-#     count = df.Overlap.tolist()
-#     count.reverse()
-#     bonferroni = -np.log(df['Adjusted P-value'])
-#     bonferroni = bonferroni.tolist()
-#     bonferroni.reverse()
-#     fig = make_subplots(
-#         rows=1, cols=2,
-#         shared_xaxes=True,
-#         vertical_spacing=0.03,
-#         specs=[[{"type": "bar"},{"type": "scatter"}]]
-#     )
+def plu_savefig(ax, save, plot_size=None, legend_out=True, right_pad_frac=0.3, mplrc=None):
+    plu.reset_mpl_style(mplrc)
+    plu.save_single_ax(ax, save, plot_size=plot_size, legend_out=True, right_pad_frac=0.3)
 
-#     fig.add_trace(go.Bar(
-#         y=term_name,
-#         x=count,
-#         name='Count',
-#         orientation='h'
-#     ))
-    
-#     fig.add_trace(
-#         go.Scatter(
-#             x=bonferroni,
-#             y=term_name,
-#             name="-log(padj)",
-#             line=dict(dash='dot')
-#         ),
-#         row=1, col=2
-#     )
-
-
-#     fig.update_layout(template='simple_white',title_text=name,xaxis = dict(showgrid=False, ticks='inside',mirror=False,showline=True),
-#                 yaxis = dict(showgrid=False, ticks='inside',mirror=False,showline=True),yaxis2=dict(side = 'right',showticklabels=False),
-#                 font=dict(size=18),
-#                   width=2000,
-#                   height=800,
-#                  title_x=0.5,
-#                  showlegend=True)
-#     fig.write_image(f'figures/{name}.pdf', scale=1, engine="kaleido", font_family = "Arial")
-
-def plot_GO_one(fname, name, term):
-    
-    df = pd.read_csv(fname,sep='\t',decimal='.')
+def barh(df, term, direction, overlap, save, return_ax=False, mplrc=None, plot_size=(1.7725, 1.6725)):
+    if isinstance(df, str):
+        df = pd.read_csv(df,sep='\t',decimal='.')
+    else:
+        df = df.copy()
     df['Adjusted P-value'].astype(float)
-    df = df.loc[df['Adjusted P-value']<0.05]
+
     if term == 'MP':
         term_name = [' '.join(segment.split(' ')[:-1]) for segment in df.Term]
     else:
         term_name = [segment.split('(')[0] for segment in df.Term]
-    #term_name.reverse()
+
     ratio = df.Overlap.apply(lambda x: eval(compile(x, '<string>', 'eval', __future__.division.compiler_flag)))
-    #count = pd.to_numeric(df.Overlap, errors='coerce').tolist()
-    #count.reverse()
+    n_overlap = [int(overlap.split('/')[0]) for overlap in df.Overlap]
+
     bonferroni = -np.log(pd.to_numeric(df['Adjusted P-value'], errors='coerce'))
-    #bonferroni.reverse()
-    
-    d = {'Term':term_name, 'Overlap':ratio, '-logp':bonferroni}
+
+    if direction == 'up':
+        cmap='YlOrRd'
+    else:
+        cmap='YlGnBu'
+    if overlap=='exact':
+        d = {'Term':term_name, 'Overlap':n_overlap, '-logp':bonferroni}
+    else:
+        d = {'Term':term_name, 'Overlap':ratio, '-logp':bonferroni}
+
     df_toplot = pd.DataFrame(data=d)
+    df_toplot = df_toplot.sort_values(by='-logp', ascending=False)
     norm = plt.Normalize(bonferroni.min(), bonferroni.max())
-    sm = plt.cm.ScalarMappable(cmap="YlOrRd", norm=norm)
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-    fig=plt.figure(figsize=(4, 3))
-    ax = sns.barplot(x='Overlap', y='Term', hue='-logp', data=df_toplot, palette='YlOrRd', dodge=False)
-    #plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    ax.get_legend().remove()
-    fig.subplots_adjust(right=0.95) # create space on the right hand side
+    ax = sns.barplot(x='Overlap', y='Term', hue='-logp', data=df_toplot, palette=cmap, dodge=False, legend=False)
     sub_ax = plt.axes([0.96, 0.55, 0.02, 0.3]) # add a small custom axis
     ax.figure.colorbar(sm, cax=sub_ax)
-    plt.savefig(f'figures/{name}.pdf', bbox_inches='tight', dpi=900, transparent=True)
+    if return_ax:
+        return ax
+    if save:
+        plu_savefig(ax, save, plot_size=plot_size, legend_out=True, right_pad_frac=0.3, mplrc=mplrc)
